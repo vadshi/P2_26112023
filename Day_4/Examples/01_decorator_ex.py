e07eed00ca6bca9d3db0b09514380f5f3fe8cfb2
Vadim Shikhovtsov <vadimx30@gmail.com>
2023-12-17 16:45:49 +0300

    Add task1 practice

--- Day_4/Examples/01_decorator_ex.py
+++ Day_4/Examples/01_decorator_ex.py
@@ -19,7 +19,7 @@ def greet():
 # Механизм работы декоратора
 # print("Before:")
 # print(greet())
-# greet = null_decorator(greet)
+# greet = null_decorator(greet)  # сам механизм
 # print("After:")
 # print(greet())
 
@@ -34,10 +34,10 @@ def uppercase(func):
 
 # Декоратор
 # @uppercase  # тоже самое, что и greet_eng = uppercase(greet_eng)
-# def greet_eng() -> str:
-#     return 'Hello!'
-#
-#
+def greet_eng() -> str:
+    return 'Hello!'
+
+
 # print(greet_eng())  # out -> <<<<HELLO!>>>>
 # print(greet_eng)
 #
@@ -50,33 +50,33 @@ def uppercase(func):
 # print(uppercase(greet))
 
 # Функция декоратор
-def other(func):
-    # Кладем все аргументы декорируемой функции
-    # в функцию wrapper
-    def wrapper(*args, **kwargs):
-        print(f'{args = }')
-        print(f'{kwargs = }')
-        original_result = func(*args, **kwargs)
-        modified_result = original_result - 100
-        return modified_result
-    return wrapper
-
-
-# Переопределяем встроенную функцию sum
+# def other(func):
+#     # Кладем все аргументы декорируемой функции
+#     # в функцию wrapper
+#     def wrapper(*args, **kwargs):
+#         print(f'{args = }')
+#         print(f'{kwargs = }')
+#         original_result = func(*args, **kwargs)
+#         modified_result = original_result * 1.5
+#         return modified_result
+#     return wrapper
+#
+#
+# # Переопределяем встроенную функцию sum
 # sum = other(sum)
 # print(sum((100, 100), start=1000))
 
 # Применение нескольких декораторов
-def strong(func):
-    def wrapper():
-        return '<strong>' + func() + '</strong>'
-    return wrapper
-
-
-def emphasis(func):
-    def wrapper():
-        return '<em>' + func() + '</em>'
-    return wrapper
+# def strong(func):
+#     def wrapper():
+#         return '<strong>' + func() + '</strong>'
+#     return wrapper
+#
+#
+# def emphasis(func):
+#     def wrapper():
+#         return '<em>' + func() + '</em>'
+#     return wrapper
 
 
 # Порядок применения снизу вверх
@@ -91,28 +91,29 @@ def emphasis(func):
 # # Без сахара
 # greet2 = strong(emphasis(greet2))
 # print(greet2())
-# #
+# print(greet2)
+
 # Функция декоратор
-# def trace(func):
-#     def wrapper(*args, **kwargs):
-#         print(f'ТРАССИРОВКА: вызвана {func.__name__}() '
-#               f'с {args}, {kwargs}')
-#         original_result = func(*args, **kwargs)
-#         print(f'ТРАССИРОВКА: {func.__name__}() '
-#               f'вернула {original_result!r}')
-#         return '!!!! ' + original_result + ' !!!!'
-#     return wrapper
-#
-#
-# @trace
-# def say(name, line):
-#     return f'{name * 3}: {line} as is'
-#
-#
-# print(say('hi', line='Hello'))
-# print('*' * 30)
-# print(say('hello', 5.2))
-# print('*' * 30)
-# print(say(line=10, name='user'))
+def trace(func):
+    def wrapper(*args, **kwargs):
+        print(f'ТРАССИРОВКА: вызвана {func.__name__}() '
+              f'с {args}, {kwargs}')
+        original_result = func(*args, **kwargs)
+        print(f'ТРАССИРОВКА: {func.__name__}() '
+              f'вернула {original_result!r}')
+        return '!!!! ' + original_result + ' !!!!'
+    return wrapper
+
+
+@trace
+def say(name, line):
+    return f'{name * 3}: {line} as is'
+
+
+print(say('hi', line='Hello'))
+print('*' * 30)
+print(say('hello', 5.2))
+print('*' * 30)
+print(say(line=10, name='user'))
 
 
